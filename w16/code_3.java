d975fb566b661730f79344dd419738e7d38e9ceb
Wei-Chin Huang <72090108+jamesD33064@users.noreply.github.com>
2021-12-28 16:51:22 +0800

    Create code_3.java

---
+++ w16/code_3.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws ArguementException {
+        Scanner s = new Scanner(System.in);
+        String str=s.next();
+        try {
+            for(int i=0 ; i<str.length() ; i++){
+                if(str.charAt(i)==' ') {
+                    throw new ArguementException(1," 空白字串");
+                }
+                else if((str.charAt(i)>='A' && str.charAt(i)<='Z')||(str.charAt(i)>='a'&&str.charAt(i)<='z')) {
+                    throw new ArguementException(2," 英文字母");
+                }
+                else if (str.charAt(i)<'0' || str.charAt(i)>'9'){
+                    throw new ArguementException(3," 符號");
+                }
+            }
+
+        } catch(Exception e) {
+            System.out.print(e.getMessage());
+        }
+    }
+}
+
+class ArguementException extends Exception {
+    int num;
+    String msg;
+    public ArguementException(int num, String msg) {
+        this.num=num;
+        this.msg=msg;
+    }
+    public String getMessage(){
+        return "ArguementException:"+Integer.toString(num)+msg;
+    }
+
+}
+
